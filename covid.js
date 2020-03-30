e363c2e4bda8fcf5ce0ae4beccb9576d19430d79
Burak Çelik <burk.clik@gmail.com>
2020-03-30 13:25:14 +0300

    version 0.0.1

---
+++ covid.js
@@ -0,0 +1,58 @@
+const {
+    Client,
+    MessageEmbed
+} = require('discord.js');
+const fetch = require('node-fetch');
+const auth = require('./auth.json');
+const countryJson = require('./countries.json');
+
+
+let tempCountry = null;
+const prefix = auth.prefix;
+
+
+// Create an instance of a Discord client
+const client = new Client();
+
+async function getData(country) {
+    const response = await fetch(auth.url + country);
+    const data = await response.json();
+    return data;
+}
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+    if (message.content.startsWith(prefix)) {
+        var country = message.content.slice(prefix.length);
+        tempCountry = countryJson[country];
+        if (message.content.startsWith(prefix + country) && tempCountry !== undefined) {
+            getData(tempCountry)
+                .then(response => {
+                    const embed = new MessageEmbed()
+                        .setThumbnail(response["countryInfo"]["flag"])
+                        .setTitle(response["country"])
+                        .setColor(0xff0000)
+                        .setDescription(`Günlük Vaka: **${response.todayCases}**
+                                Günlük Vefat: **${response.todayDeaths}**
+                                Kritik Vaka: **${response.critical}**
+                                İyileşen: **${response.recovered}**
+                                Toplam Vaka: **${response.cases}**
+                                Toplam Vefat: **${response.deaths}**`)
+                        .setTimestamp(response["updated"])
+                    message.channel.send(embed);
+                }).catch(err => console.log(err));
+        } else if (tempCountry === undefined) {;
+            message.channel.send('Düzgün yaz şunu mübarek');
+        }
+    }
+});
+
+client.login(auth.token);
